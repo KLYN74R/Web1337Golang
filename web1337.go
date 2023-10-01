73f1eb1ee3fdf510e0fe4daf7b83a29d31e74264
Vlad Chernenko <vladchernenko@protonmail.com>
2023-10-02 01:10:03 +0300

    Fix

---
+++ web1337.go
@@ -0,0 +1,15 @@
+/*
+
+Web1337 by KLY
+
+For Golang devs
+
+*/
+
+package web1337
+
+import "fmt"
+
+func Hello() {
+	fmt.Println("Hello from Web1337")
+}
--- web1337_test.go
+++ web1337_test.go
@@ -5,11 +5,3 @@ Web1337 by KLY
 For Golang devs
 
 */
-
-package web1337
-
-import "fmt"
-
-func Hello() {
-	fmt.Println("Hello from Web1337")
-}
